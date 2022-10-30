d5f3fa706a757b134a9a1e2bc02a2231214487e2
Darkshadow0001boss <76279667+Darkshadow0001boss@users.noreply.github.com>
2022-10-30 15:13:52 +0530

    Create numPattern.cpp
    This program gives you a pattern of numbers when you enter the number

---
+++ numPattern.cpp
@@ -0,0 +1,42 @@
+// Github username: Darkshadow0001boss
+// Github: https://github.com/Darkshadow0001boss
+// Aim: This program gives you a pattern of numbers when you enter the number.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        // space
+        int sp = n - i;
+        while (sp > 0)
+        {
+            cout << " ";
+            sp--;
+        }
+
+        // nos.
+        int j = 1;
+        while (j <= i)
+        {
+            cout << j;
+            j++;
+        }
+
+        // 2nd nos.
+        int m = i - 1;
+        while (m > 0)
+        {
+            cout << m;
+            m--;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
